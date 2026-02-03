6d37ceb6d52f29a9f3e8ce1527d995633e9e79d4
rivaldo <rivaldo-sp@hotmail.com>
2026-02-03 01:00:17 -0300

    so scriptjs ws

--- script.js
+++ script.js
@@ -4083,7 +4083,7 @@ if (idDoEvento) {
     console.error("❌ init: ID do evento não encontrado.");
 }
 
-        // connectWebSocket();   //  sem par
+        connectWebSocket();   //  testando sem esta função
         setInterval(() => {
             // Só verifica se o cliente estiver logado e o jogo já tiver começado (não estiver na animação de início)
             if (typeof clienteLogado !== 'undefined' && clienteLogado && !iniciandoRodada) {
